d165c1646cb8dfadf317aeadd79f977fd30699f1
mlu <margaretlu.2018@gmail.com>
2024-04-29 16:25:58 -0400

    update bkgd url part 4

--- js/main.js
+++ js/main.js
@@ -917,7 +917,7 @@ function showResult() {
         console.log(res.color.opacity);
         console.log(resOpacity.classList.add(res.color.opacity));
 
-        let bkgdURL = "url(\"../../chairacter/" + res.image + "\")";
+        let bkgdURL = "url(\"../../chairacter/" + res.image + "\"";
         // ../../chairacter/assets/infj-bibendum.png
         console.log(bkgdURL);
 
