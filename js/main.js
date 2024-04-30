e62c0140f1f716f2fec7bc8e6f42c5b5e1f1a405
mlu <margaretlu.2018@gmail.com>
2024-04-29 20:14:45 -0400

    changed question 8

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@ const questions = [
     },
     {
         questionNumber: "8",
-        question: "All done! You are happy with your purchase; now you are strolling down the parking lot to your car, discussing how to decorate your new home with the furniture you bought. But... wait a minute, uh oh! As soon as you open your car's trunk, you and your friend realize that there is so little space for all of the furniture to fit in. How would you fit all the furniture that you just bought?",
+        question: "All done! Happily strolling down the parking lot with the furniture just bought. But... wait a minute, uh oh! Upon opening your car's trunk, you and your friend realize there's little space for all the furniture. What would be your immediate reaction?",
         image: "https://images.unsplash.com/photo-1634712282287-14ed57b9cc89?q=80&w=3012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         answers: {
             F: {
